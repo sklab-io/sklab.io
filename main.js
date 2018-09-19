7fd7c403296955e1d295558d74cf4d3462611e6e
Spencer Rudnick <therudnick@gmail.com>
2018-09-18 22:19:40 -0500

    log AudioContext for play

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-((msg) => {
-  window.addEventListener('load', () => {
-    console.log(msg);
-  });
-})('site loaded');
+try {
+  ((msg) => {
+    const ctx = new AudioContext();
+    window.addEventListener('load', () => {
+      console.log(msg);
+      console.log(ctx);
+    });
+  })('site loaded');
+} catch (e) {
+  console.error(e);
+}
