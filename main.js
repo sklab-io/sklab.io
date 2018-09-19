e1cd27176a36752354999d65b1aa2b8ef2818bc8
Spencer Rudnick <therudnick@gmail.com>
2018-09-18 22:50:24 -0500

    add exception handling to page load

--- main.js
+++ main.js
@@ -1,29 +1,28 @@
 
+
+
 try {
   var context = new AudioContext();
-  const init = (msg) => () => {
-    try {
-      console.log(msg);
-      throw msg;
-    } catch (e) {
-      console.error(e);
-      alert(`
+  const exceptionAlert = (e) => {
+    console.error(e);
+    alert(`
         Seems like your browser doesn't support ES6.
         
         Check out a modern browser like FireFox (firefox.com) or Chrome (chrome.com).
         
         <3`);
+  };
+  const init = (msg) => () => {
+    try {
+      console.log(msg);
+      throw msg;
+    } catch (e) {
+      exceptionAlert();
     }
   };
   ((msg) => {
     window.addEventListener('load', init(msg));
   })('site loaded');
 } catch (e) {
-  console.error(e);
-  alert(`
-    Seems like your browser doesn't support ES6 or the Web Audio API.
-    
-    Check out a modern browser like FireFox (firefox.com) or Chrome (chrome.com).
-    
-    <3`);
+  exceptionAlert(e);
 }
