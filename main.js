25314763deb69ebcdc0b5a3f017f005c06220215
Spencer Rudnick <therudnick@gmail.com>
2018-09-18 22:41:15 -0500

    add global error handling plus a test

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 
-var context = new AudioContext();
 
 try {
+  var context = new AudioContext();
   ((msg) => {
     window.addEventListener('load', () => {
       console.log(msg);
+      if (typeof msg === 'string') {
+        throw "UH OH";
+      }
+      console.log(``)
     });
   })('site loaded');
 } catch (e) {
   console.error(e);
+  alert(`
+    Seems like your browser doesn't support ES6 or the Web Audio API.
+    
+    Check out a modern browser like FireFox (firefox.com) or Chrome (chrome.com).
+    
+    <3`);
 }
