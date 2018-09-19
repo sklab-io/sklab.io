28ad8b7f13e17ee1a10bad7199d27ca76bc8e343
Spencer Rudnick <therudnick@gmail.com>
2018-09-19 00:47:52 -0500

    Revert "messing around lol"
    This reverts commit dd92a93acb55457a1bd32896fbc062414c96f01f.

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
 
 /** GLOBAL SITE SCRIPTS **/
 
-/// Declare global objects in other files.
-/// Add src links above references to this file.
-((msg) => {
-  tryCatch(msg, (() => {
-    const init = () => {
-      try {
-        console.log(msg);
-      } catch (e) {
-        handle(e);
-      }
-    };
-    window.addEventListener('load', init);
-  })).bind(this);
-})(`
-Looks like your browser doesn't support ES6 or the Web Audio API.
-Why not try out a modern browser?
+/// Declare global objects in other files
+/// and add them to index.html.
 
-- firefox.com
-- chrome.com
+const handle = customError(`
+  Looks like your browser doesn't support ES6 or the Web Audio API.
+  Why not try out a modern browser?
 
-<3`).bind(this);
+  - firefox.com
+  - chrome.com
+
+  <3`);
+
+try {
+  const init = (msg) => () => {
+    try {
+      console.log(msg);
+    } catch (e) {
+      handle(e);
+    }
+  };
+  ((msg) => {
+    window.addEventListener('load', init(msg));
+  })('site loaded');
+} catch (e) {
+  handle(e);
+}
--- play/context.js
+++ play/context.js
@@ -1,12 +1,15 @@
-((msg) => {
-  tryCatch(msg)((() => {
-    var context = new AudioContext();
-  }).bind(this));
-})(`
-Looks like your browser doesn't support the Web Audio API.
-Why not try out a modern browser?
+const handle = customError(`
+  Looks like your browser doesn't support the Web Audio API.
+  Why not try out a modern browser?
 
-- firefox.com
-- chrome.com
+  - firefox.com
+  - chrome.com
 
-<3`).bind(this);
+  <3`);
+
+try {
+  var context = new AudioContext();
+  throw context;
+} catch (e) {
+  handle(e);
+}
